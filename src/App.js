2544cddca4b297ee77c2d4df36bfe1ffce0a551a
Hakizu <hakisuj@gmail.com>
2020-06-15 12:05:41 +0200

    FIX: 5.1: bloglist frontend, step1

--- src/App.js
+++ src/App.js
@@ -69,12 +69,11 @@ const App = () => {
         loginForm() :
         <div>
           <p>{user.name} logged in </p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
         </div>
       }
-
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
     </div>
   )
 }
